7ff4402ee4ffa82a9b4984ddb9dd90b7939d5571
tolgayakar97 <tolgayakar97@gmail.com>
2026-08-26 21:16:22 +0300

    spring boot backend created and dockerized

---
+++ backend/src/main/java/com/tolgayakar/receipt_manager/ReceiptManagerApplication.java
@@ -0,0 +1,13 @@
+package com.tolgayakar.receipt_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiptManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceiptManagerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/tolgayakar/receipt_manager/ReceiptManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tolgayakar.receipt_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiptManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
